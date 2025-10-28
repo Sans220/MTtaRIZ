aa9d85a389058ea1c00de8bd35352577c228cfe9
Sans220 <agentsteve3.0@gmail.com>
2025-10-29 01:14:51 +0200

    Додано ще один рядок тексту

--- Lab1/Lab1.java
+++ Lab1/Lab1.java
@@ -2,5 +2,6 @@ class Lab1 {
     public static void main(String[] args) {
         System.out.println("It works");
         System.out.println("Second line");
+        System.out.println("Second line X2");
     }
 }
