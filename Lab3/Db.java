62e1747b56f0aa03dd4ebfdb7c1cd738dedd6fd1
Sans220 <agentsteve3.0@gmail.com>
2025-12-19 09:21:30 +0200

    editing

--- Lab3/Db.java
+++ Lab3/Db.java
@@ -1,4 +1,8 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Db {
     private static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/?user=root";
